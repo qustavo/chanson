d9baaf219e580792d913af2c574ce5e8b3bbc21f
Gustavo Chain <gchain@gmail.com>
2015-10-31 18:04:14 +0100

    Add benchmark

--- chanson_test.go
+++ chanson_test.go
@@ -3,7 +3,9 @@ package chanson
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"strings"
 	"testing"
 
@@ -120,4 +122,37 @@ func TestWithChannels(t *testing.T) {
 	  "bool": [true,false]
   	}`), trim(buf.String()))
 
+}
+
+func buildLargeObject(size int) map[string]string {
+	size = size * 900000
+
+	obj := make(map[string]string, size)
+
+	for i := 0; i < size; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		val := fmt.Sprintf("val-%d", i)
+		obj[key] = val
+	}
+	return obj
+}
+
+func BenchmarkNativeEncoder(b *testing.B) {
+	lo := buildLargeObject(b.N)
+	b.ResetTimer()
+
+	json.NewEncoder(ioutil.Discard).Encode(lo)
+
+}
+
+func BenchmarkChanson(b *testing.B) {
+	lo := buildLargeObject(b.N)
+	b.ResetTimer()
+
+	cs := New(ioutil.Discard)
+	cs.Object(func(obj Object) {
+		for k, v := range lo {
+			obj.Set(k, v)
+		}
+	})
 }
