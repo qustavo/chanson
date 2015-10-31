4e652bf1c7a9b20497b20b66d8e66f6e960ca45b
Gustavo Chain <gchain@gmail.com>
2015-10-31 22:47:43 +0100

    Move benchmark into a different file

---
+++ chanson_benchmarks_test.go
@@ -0,0 +1,41 @@
+package chanson
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func buildLargeObject(size int) map[string]string {
+	size = size * 900000
+
+	obj := make(map[string]string, size)
+
+	for i := 0; i < size; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		val := fmt.Sprintf("val-%d", i)
+		obj[key] = val
+	}
+	return obj
+}
+
+func BenchmarkNativeEncoder(b *testing.B) {
+	lo := buildLargeObject(b.N)
+	b.ResetTimer()
+
+	json.NewEncoder(ioutil.Discard).Encode(lo)
+
+}
+
+func BenchmarkChanson(b *testing.B) {
+	lo := buildLargeObject(b.N)
+	b.ResetTimer()
+
+	cs := New(ioutil.Discard)
+	cs.Object(func(obj Object) {
+		for k, v := range lo {
+			obj.Set(k, v)
+		}
+	})
+}
